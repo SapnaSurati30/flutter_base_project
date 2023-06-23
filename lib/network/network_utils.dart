38d0de79a6afe6583365e5f37474f606a48c94af
sapnasurati30 <sapnasurati30@gmail.com>
2023-06-23 23:38:38 +0530

    - fix issue

--- lib/network/network_utils.dart
+++ lib/network/network_utils.dart
@@ -100,3 +100,100 @@ Future handleResponse(Response response, {HttpResponseType responseType = HttpRe
     }
   }
 }
+
+
+//
+//
+// Map<String, String> buildHeaderTokens() {
+//   Map<String, String> header = {
+//     HttpHeaders.contentTypeHeader: 'application/json; charset=utf-8',
+//     HttpHeaders.cacheControlHeader: 'no-cache',
+//     HttpHeaders.acceptHeader: 'application/json; charset=utf-8',
+//     'Access-Control-Allow-Headers': '*',
+//     'Access-Control-Allow-Origin': '*',
+//   };
+//
+//   print(jsonEncode(header));
+//   return header;
+// }
+//
+// Uri buildBaseUrl(String endPoint) {
+//   Uri url = Uri.parse(endPoint);
+//   if (!endPoint.startsWith('http')) url = Uri.parse('$BASE_URL$endPoint');
+//
+//   print('URL: ${url.toString()}');
+//
+//   return url;
+// }
+//
+// Future<Response> buildHttpResponse(String endPoint, {HttpMethod method = HttpMethod.GET, Map? request}) async {
+//   if (await isNetworkAvailable()) {
+//     var headers = buildHeaderTokens();
+//     Uri url = buildBaseUrl(endPoint);
+//
+//     Response response;
+//
+//     if (method == HttpMethod.POST) {
+//       print('Request: $request');
+//       response = await http.post(url, body: jsonEncode(request), headers: headers);
+//     } else if (method == HttpMethod.DELETE) {
+//       response = await delete(url, headers: headers);
+//     } else if (method == HttpMethod.PUT) {
+//       response = await put(url, body: jsonEncode(request), headers: headers);
+//     } else {
+//       response = await get(url, headers: headers);
+//     }
+//
+//     print('Response ($method): ${response.statusCode} ${response.body}');
+//
+//     return response;
+//   } else {
+//     throw "errorInternetNotAvailable";
+//   }
+// }
+//
+// @deprecated
+// Future<Response> getRequest(String endPoint) async => buildHttpResponse(endPoint);
+//
+// @deprecated
+// Future<Response> postRequest(String endPoint, Map request) async => buildHttpResponse(endPoint, request: request, method: HttpMethod.POST);
+//
+// Future handleResponse(Response response) async {
+//   if (!await isNetworkAvailable()) {
+//     throw "Internet not available";
+//   }
+//
+//   if (response.statusCode.isSuccessful()) {
+//     return jsonDecode(response.body);
+//   } else {
+//     var string = await (isJsonValid(response.body));
+//     if (string!.isNotEmpty) {
+//       throw string;
+//     } else {
+//       throw 'Please try again later.';
+//     }
+//   }
+// }
+//
+// //region Common
+// enum HttpMethod { GET, POST, DELETE, PUT }
+//
+// class TokenException implements Exception {
+//   final String message;
+//
+//   const TokenException([this.message = ""]);
+//
+//   String toString() => "FormatException: $message";
+// }
+// //endregion
+//
+// Future<String?> isJsonValid(json) async {
+//   try {
+//     var f = jsonDecode(json) as Map<String, dynamic>;
+//     return f["msg"];
+//   } catch (e) {
+//     print(e.toString());
+//     return "";
+//   }
+// }
+
